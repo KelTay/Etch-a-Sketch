5f5e724d0febbf4e32e1e1cefea2d45e350834f4
kelvin <klvno.tay@gmail.com>
2019-05-14 14:55:25 -0700

    Create 16 x 16 grid with JS

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 const divGrid = document.querySelector("#div-grid");
 
+function drawGrid() {
+    let row = 1;
+    let column = 1;
+
+    for (let i = 1; i <= (16 * 16); ++i) {
+
+        let divItem = document.createElement("div");
+        divItem.setAttribute("class", "grid-cell");
+        
+        divItem.textContent = "test" + i;
+
+        if (i % 16 !== 0) {
+
+            divItem.style.gridColumnStart = column;
+            divItem.style.gridColumnEnd = column + 1;
+            divItem.style.gridRowStart = row;
+            divItem.style.gridRowEnd = row + 1;
+
+            ++column;
+
+        } else {
+
+            ++row;
+            column = 1;
+        }
+
+        divGrid.appendChild(divItem);
+    }
+}
+
+
+
+
+window.onload = drawGrid; // Allows function drawGrid to be
+                          // called on page load.
